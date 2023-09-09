e34debe163bea7b04eece2703ce485562f64146e
Andrii Chychkan <mywork1728@gmail>
2023-09-09 21:16:49 +0300

    started

--- App.js
+++ App.js
@@ -1,20 +1,7 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+// import { StatusBar } from "expo-status-bar";
+// import { StyleSheet, Text, View } from "react-native";
+import RegistrationScreen from "./Screens/RegistrationScreen";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>piska</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <RegistrationScreen />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "green",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
